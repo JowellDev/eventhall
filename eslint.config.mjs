c7590e39f01f8f36665feee837f9b8a0ccf2f1fd
Joël Ephraïm Digbeu <87069318+JoelDigbeu@users.noreply.github.com>
2026-04-20 14:44:02 +0000

    add eslint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "react/self-closing-comp": "warn",
+      "prefer-const": "error",
+    },
+  },
+  {
+    ignores: [".next/", "node_modules/", "dist/"],
+  },
+];
+
+export default eslintConfig;
